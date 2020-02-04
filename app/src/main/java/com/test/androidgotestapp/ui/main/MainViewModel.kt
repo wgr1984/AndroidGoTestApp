f09269c41df64b881302e0553cf8cdfc4d9f7ef4
Wolfgang Reithmeier <wolfgang.reithmeier@bmw.de>
2020-02-04 15:02:10 +0100

    use async callback

--- app/src/main/java/com/test/androidgotestapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/test/androidgotestapp/ui/main/MainViewModel.kt
@@ -1,14 +1,21 @@
 package com.test.androidgotestapp.ui.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import gotestlib.PhotosCallback
 
 class MainViewModel : ViewModel() {
     val api = gotestlib.Api()
 
     fun loadPhotos() {
-        val photos = api.photos
-        for (i in 0..photos.itemsCount) {
-            println(photos.getItem(i)?.title)
+        api.getPhotos { photos, err ->
+            if (err != null) {
+                Log.e(MainViewModel::class.simpleName, "load error", err)
+                return@getPhotos
+            }
+            for (i in 0..photos.itemsCount) {
+                println(photos.getItem(i)?.title)
+            }
         }
     }
 }
